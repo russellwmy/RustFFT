651bf339e7b502117699bfd6fd3531f7479edcb7
Piotr Fila <piotr.fila.stud@pw.edu.pl>
2023-12-13 21:34:44 +0100

    Use hashbrown instead of no-std-compat

--- src/math_utils.rs
+++ src/math_utils.rs
@@ -540,7 +540,7 @@ mod unit_tests {
         }
     }
 
-    use no_std_compat2::collections::HashMap;
+    use hashbrown::HashMap;
 
     macro_rules! map{
         { $($key:expr => $value:expr),+ } => {
